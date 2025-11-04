2ef78a129f474545c2880d6623f5177c975d82af
JM <josemiguel.sevilla@abbott.com>
2025-11-04 07:20:53 -0600

    @escreen-tms #Create parse html component

--- packages/core-lib/business/regex.ts
+++ packages/core-lib/business/regex.ts
@@ -3,6 +3,11 @@ const REGEX_ALPHA_NUMBER = /([a-z0-9])([A-Z])/g;
 const REGEX_ALPHA = /([A-Z])([A-Z][a-z])/g;
 const REGEX_NUMBER_BETWEEN = /\b\d+\b/g;
 const REGEX_WORDS = /\b\w/g;
+const MATCH_HTML_HEADINGS = /<h\d>(.+)<\/h\d>/gi;
+const MATCH_LI_WITH_DOUBLE_BRACKETS =
+  /<li>(?:(?!<\/li>).)*?\[\[.*?\]\](?:(?!<\/li>).)*?<\/li>/g;
+const MATCH_EMPTY_LI = /<li>\s*<\/li>/g;
+const MATCH_BLANK_P_TAG = /<p>\s<\/p>/gi;
 
 export {
   REGEX_CHAR_DOT,
@@ -10,4 +15,8 @@ export {
   REGEX_ALPHA,
   REGEX_NUMBER_BETWEEN,
   REGEX_WORDS,
+  MATCH_BLANK_P_TAG,
+  MATCH_EMPTY_LI,
+  MATCH_LI_WITH_DOUBLE_BRACKETS,
+  MATCH_HTML_HEADINGS,
 };
