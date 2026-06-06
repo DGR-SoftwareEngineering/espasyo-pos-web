23067acb87f8331e3a9d79448ff995cd99d76fd7
jmsevilla1 <jmsevilla@glasslewis.com>
2026-06-06 15:17:31 +0800

    @Bugfix #Fix errors in deployed application v5

--- apps/espasyo-pos/src/pages/_document.tsx
+++ apps/espasyo-pos/src/pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const nonce = (ctx.req?.headers["x-nonce"] as string) ?? "";
+    return { ...initialProps, nonce };
+  }
+
+  render() {
+    const { nonce } = this.props as typeof this.props & { nonce?: string };
+    return (
+      <Html lang="en">
+        <Head nonce={nonce} />
+        <body className="antialiased">
+          <Main />
+          <NextScript nonce={nonce} />
+        </body>
+      </Html>
+    );
+  }
 }
